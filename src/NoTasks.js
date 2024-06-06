dd10426c6fd2087bb7ef2ece125b07abcf8756dd
uxzh <77202660+uxzh@users.noreply.github.com>
2024-06-06 11:32:23 +0400

    Update v1.1

---
+++ src/NoTasks.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function NoTasks() {
+  return (
+    <div className="notasks">
+      <img src="https://imgur.com/bsqRgEb.png" alt="Add Your First Task!" />
+      <h2>Add Your First Note!</h2>
+    </div>
+  );
+}
